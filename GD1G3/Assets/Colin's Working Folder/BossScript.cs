9ceaff04d5d8daf28ab4636708882d06102d99e2
Colin <cehiggins0@gmail.com>
2018-11-02 16:45:03 -0400

    boss rotates with mouse position
    Pending adjustment of boss model for finalization

--- GD1G3/Assets/Colin's Working Folder/BossScript.cs
+++ GD1G3/Assets/Colin's Working Folder/BossScript.cs
@@ -1,29 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
 
 public class BossScript : MonoBehaviour
 {
-    public int health;
-    public int energy;
+    public int healthMax;
+    public int energyMax;
     public float speed = 3.0F;
     private Rigidbody rb;
+    private int currentHealth;
+    private int currentEnergy;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentHealth = healthMax;
+        currentEnergy = energyMax;
     }
+    float lastTime = 0;
+    float lastAngle = 0;
     void Update()
     {
         //Debug.Log(transform.position);
         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity[1], Input.GetAxis("Vertical") * speed);
-        if (health == 0)
+        if (currentHealth == 0)
         {
             //endgame
         }
-        if (energy < 100)
+        if (currentEnergy < 100)
         {
-            Debug.Log(Time.time);
+            if (Time.time > lastTime + 0.1)
+            {
+                lastTime = Time.time;
+                currentEnergy += 1;
+            }
         }
+        Vector2 mouseLocation = Input.mousePosition;
+        double newAngle = GetDirection(mouseLocation[0], mouseLocation[1]);
+        transform.localEulerAngles = new Vector3(0, (float)newAngle, 0);
+    }
 
+    double GetDirection(float x, float y)
+    {
+        Vector3 gameLocation = transform.localPosition;
+        Vector2 screenCoordinates = Camera.main.WorldToScreenPoint(gameLocation);
+        double ydistance = screenCoordinates[1] - y;
+        double xdistance = screenCoordinates[0] - x;
+        double angleToMouse = Math.Atan2(xdistance, ydistance);
+        angleToMouse *= 180 / Math.PI;
+        return angleToMouse;
     }
 
 }
