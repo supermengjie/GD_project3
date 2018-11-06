6b01a3b83896f6c3f59e71e9e4ffe69bf25c7adb
Unknown <yem@rpi.edu>
2018-11-06 18:12:15 -0500

    add menu scene pause scene, hero projectile

---
+++ GD1G3/Assets/scenescript/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(IsGamePaused)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("loading menu");
+        SceneManager.LoadScene("menu");
+    }
+    public void quitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
